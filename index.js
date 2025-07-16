842b7b26404f388d61d0cd754723ca03c59f60b6
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-16 22:05:50 +0530

    code

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
-a=10
-print(a)
+const express = require(express);
+const app = express();
+
+app.post("/",function(req, res){
+    res.json({
+        message: ""
+    })
+})
+app.post("/", function(req, res){
+    res.json({
+        message: ""
+    })
+})
+app.get("/",function(req, res){
+     res.json({
+        message: ""
+    })
+})
+
+app.get("/",function(req, res){
+     res.json({
+        message: ""
+    })
+})
+
+pp.get("/",function(req, res){
+     res.json({
+        message: ""
+    })
+})
+
