4218e76d0052f0db5400b1d164c0060dd6367d76
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-17 12:16:26 +0530

    code

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/v1/user", userRouter);
+app.use("/api/v1/admin", userRouter);
 app.use("/api/v1/course", courseRouter); // no error now
 
 app.listen(3000, () => {
