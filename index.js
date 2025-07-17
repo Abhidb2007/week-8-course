0c94ae796084e2c1becddca5bb12c45af337bf5e
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-17 12:13:51 +0530

    code

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const { userRouter } = require("./routes/user");
-const { CourseRouter } = require("./routes/course");
+const { courseRouter } = require("./routes/course"); // match export name
 
 const app = express();
 
-app.use(express.json()); // optional, needed if you want to parse JSON body
+app.use(express.json());
 
 app.use("/api/v1/user", userRouter);
-app.use("/api/v1/course", CourseRouter);
+app.use("/api/v1/course", courseRouter); // no error now
 
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
