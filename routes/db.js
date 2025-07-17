72d3f572d346ad11d4809997e3d68789bc7420ef
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-17 15:56:38 +0530

    code

--- routes/db.js
+++ routes/db.js
@@ -1,20 +1,37 @@
 const {Schema,default: mongoose} = require("mongoose");
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
  
 const userSchema = Schema({
-    email
+    email: {type: String,unique: true},
+    password: String,
+    firstname: String,
+    lastname: String,
 
 });
 
 
 const adminSchema = Schema({
+    email: {type: String,unique: true},
+    password: String,
+    firstname: String,
+    lastname: String,
 
 });
 
 const courseSchema = Schema({
+    description : String,
+    title: String,
+    imageurl: String,
+    creatorId: ObjectId,
+    price:Number
 
 });
 
 const purchaseSchema = Schema({
+    userId: ObjectId,
+    courseId: ObjectId
+
 
 });
 
