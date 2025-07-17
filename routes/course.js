9b2595c8143ac828978ea7512b6243698fcb9a4d
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-17 11:45:30 +0530

    code

--- routes/course.js
+++ routes/course.js
@@ -1,19 +1,18 @@
 const { Router } = require("express");
 const courseRouter = Router();
-    courseRouter.post("/purchase", function(req, res){
-        res.json({
-            message: "signup endpoint "
-        })
-    })
 
-    courseRouter.get("/preview", function(req, res){
-        res.json({
-            message: "signup endpoint "
-        })
-    })
+courseRouter.post("/purchase", function (req, res) {
+  res.json({
+    message: "Purchase endpoint",
+  });
+});
 
+courseRouter.get("/preview", function (req, res) {
+  res.json({
+    message: "Preview endpoint",
+  });
+});
 
 module.exports = {
-    courseRouter:  courseRouter
-}
-    
+  courseRouter, // 👈 use this if you're importing with lowercase in index.js
+};
