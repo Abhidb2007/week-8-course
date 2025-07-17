d3757cdf40273c7e3f7078d680d56813c2aecee0
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-17 12:25:43 +0530

    code

---
+++ routes/admin.js
@@ -0,0 +1,37 @@
+const { Router } = require("express");
+const adminRouter = Router();
+
+adminRouter.post("/signup", function (req, res) {
+  res.json({
+    message: "Signup endpoint",
+  });
+});
+
+adminRouter.post("/signin", function (req, res) {
+  res.json({
+    message: "Signup endpoint",
+  });
+});
+adminRouter.get("/course", function (req, res) {
+  res.json({
+    message: "signup endpoint",
+  });
+}); 
+
+adminRouter.get("/course", function (req, res) {
+  res.json({
+    message: "signup endpoint",
+  });
+}); 
+
+adminRouter.get("/course/bulk", function (req, res) {
+  res.json({
+    message: "signup endpoint",
+  });
+}); 
+
+module.exports = {
+  userRouter,
+};
+
+
