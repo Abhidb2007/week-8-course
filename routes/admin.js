b17b04403abfe97fe310194111f21f28d9ea4ff5
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-18 13:32:47 +0530

    code

--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,44 @@
 const { Router } = require("express");
 const adminRouter = Router();
+const { adminModel } = require("./db");
+
 
 // Admin signup
 adminRouter.post("/signup", function (req, res) {
-  res.json({
-    message: "Admin signup endpoint",
-  });
-});
+  const { email, password, firstname, lastname } = req.body;
+  await userModel.create({
+        email,
+        password: hashedPassword,
+        firstname,
+        lastname,
+      });
+  
+      res.json({
+        message: "Signup successfully",
+    });
+})
 
 // Admin signin
 adminRouter.post("/signin", function (req, res) {
-  res.json({
-    message: "Admin signin endpoint",
-  });
+  userRouter.post("/signin", async function (req, res) {
+    const{email, password} = req.body;
+    // TODO: ideally password should be hashed, and hence you compare the user provided password and the database password
+    const user = await userModel.findOne({
+      password: password,
+      email: email
+    })
+    if(user){
+      const token: Jwt.sign({
+        id: user._id
+      },JWT_USER_PASSWORD);
+      res.json({
+        token: token
+      })
+    }else{
+      res.status(403).json{
+        message: "Incorrect crediantials"
+      }
+    }
 });
 
 // Get all courses created by admin
