129adc5a48c33bd844a7b4180e118a9a5f5f3468
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-20 10:41:23 +0530

    code

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose =  require("mongoose");
 const cors = require("cors");
+const { purchaseModel } = require("./routes/db");
 const jwt = ("jsonwebtoken");
 const app = express();
 app.use = (express.json());
@@ -9,6 +10,10 @@ const SECRET = "secret";
 //middleware Models
 const userSchema = new mongoose.Schema({
   username: String,
+  password: String,
+  purchasedCourse: ({type:monggose.Schema.Type.ObjectId,ref: "course"})
+
 
 })
+
 
